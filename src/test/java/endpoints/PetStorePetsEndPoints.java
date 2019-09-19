467b173437f9f50f62008d775a84096cc118b8e7
Andi3z <gremo77t@gmail.com>
2019-09-19 17:44:12 +0300

    added findByStatus refactor PetsEndPoints

--- src/test/java/endpoints/PetStorePetsEndPoints.java
+++ src/test/java/endpoints/PetStorePetsEndPoints.java
@@ -7,20 +7,28 @@ import utils.PropertiesController;
 
 import static io.restassured.RestAssured.given;
 
-public class PetStorePetsEndPoint {
-
+public class PetStorePetsEndPoints {
+    private static final String FIND_PETS = PropertiesController.getProperty("petstore.get.petsby.status");
     private static final String ADD_NEW_PET = PropertiesController.getProperty("petstore.add.pet");
+
+    public Response getPetsByStatus(String status){
+        return given()
+                .param("status", status)
+                .when()
+                .get(FIND_PETS);
+    }
 
     public Response addPet(Pet pet) {
         Response response = given()
-                            .header("Content-type", "application/json")
-                            .body(pet)
-                        .when()
-                            .post(ADD_NEW_PET);
+                .header("Content-type", "application/json")
+                .body(pet)
+                .when()
+                .post(ADD_NEW_PET);
         response
                 .then()
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK);
-        return null;
+        return response;
     }
+
 }
--- src/test/java/tests/PetStorePetsTests.java
+++ src/test/java/tests/PetStorePetsTests.java
@@ -1,14 +1,17 @@
 package tests;
 
-import endpoints.PetStorePetsEndPoint;
+import endpoints.PetStorePetsEndPoints;
 import io.restassured.response.Response;
 import models.Pet.Pet;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PetStorePetsTests extends BaseTest {
 
-    private static final PetStorePetsEndPoint PET_STORE_PET_POINTS = new PetStorePetsEndPoint();
+    private static final PetStorePetsEndPoints PET_STORE_PET_POINTS = new PetStorePetsEndPoints();
 
     @Test
     public void AddANewPetToTheStore() {
@@ -16,6 +19,18 @@ public class PetStorePetsTests extends BaseTest {
         Pet pet = Pet.createDefaultPet();
         Response petFromResponse = PET_STORE_PET_POINTS.addPet(pet);
         SoftAssert assertions = new SoftAssert();
+    }
 
+    @Test
+    public void getPetsByStatus(){
+        String status = "available";// available, pending, sold
+        Response response = PET_STORE_PET_POINTS.getPetsByStatus(status);
+        List<Pet> pets = new ArrayList<>();
+        pets = response.jsonPath().getList(".", Pet.class);
+
+        SoftAssert assertions = new SoftAssert();
+        assertions.assertEquals(response.getStatusCode(), 200);
+        assertions.assertTrue(pets.size() > 6000);
+        assertions.assertAll();
     }
 }
