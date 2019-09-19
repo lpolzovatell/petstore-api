f318d9eecb7dacaf1c08718827887fb3fd0771ed
Alexandr Schekotov <alexandrschekotov931201@gmail.com>
2019-09-19 17:22:05 +0300

    Add test post request "Add pet"

---
+++ src/main/java/models/Pet/Category.java
@@ -0,0 +1,29 @@
+package models.Pet;
+
+class Category {
+
+    private float id;
+    private String name;
+
+
+    // Getter Methods
+
+    public float getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // Setter Methods
+
+    public void setId(float id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- src/test/java/tests/PetStorePetsTests.java
+++ src/test/java/tests/PetStorePetsTests.java
@@ -1,4 +1,21 @@
 package tests;
 
+import endpoints.PetStorePetsEndPoint;
+import io.restassured.response.Response;
+import models.Pet.Pet;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 public class PetStorePetsTests extends BaseTest {
+
+    private static final PetStorePetsEndPoint PET_STORE_PET_POINTS = new PetStorePetsEndPoint();
+
+    @Test
+    public void AddANewPetToTheStore() {
+
+        Pet pet = Pet.createDefaultPet();
+        Response petFromResponse = PET_STORE_PET_POINTS.addPet(pet);
+        SoftAssert assertions = new SoftAssert();
+
+    }
 }
